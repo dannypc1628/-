74ed40329325f6c0d0bfbe0d3d817cc6f8c9e3dc
dannypc1628 <h152074716@gmail.com>
2015-10-13 12:48:05 +0800

    改好友列表

--- js/getFriend.js
+++ js/getFriend.js
@@ -15,23 +15,18 @@ function getFriendList(){
 				dataType:"JSONP",
 				jsonpCallback:"friendList",
 				success:function(returnData){
-					if(returnData.status=="200"){
-						
+					
+					var friendData = returnData.data;
+					
 						$("#friendTable").append("<tbody>");
-						for(var i = 0 ;i<monsterList.length;i++){
+						for(var i = 0 ;i<friendData.length;i++){
 							
-							$("#friendTable").append("	<tr><td>"+monsterList[i].monsterID
-								+"</td><td>"+monsterList[i].monsterName+"</td><td>"
-								+monsterList[i].monsterHP+"</td></tr>");		
+							$("#friendTable").append("	<tr><td>"+friendData[i]["username"]
+								+"</td><td>"+monsterList[i]["uid"]+"</td><td>"
+								+monsterList[i]["capital"]["name"]+"</td></tr>");		
 						}
 						$("#friendTable").append("</tbody>");
 						
-					}
-					
-					else{
-						alert("失敗。 錯誤代碼 = "+returnData.status+" 錯誤訊息="+returnData.msg);
-					}
-
 				}
 			});
 		}

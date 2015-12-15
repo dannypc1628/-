e40728d5952016220d998befcd62df2793f4a2f5
pei0722 <tonyron7839@gmail.com>
2015-12-15 22:47:20 +0800

    敵人技能 禁止放生隊長

--- js/getBox.js
+++ js/getBox.js
@@ -1,4 +1,4 @@
-
+var leaderbid;
 var bagList;
 $(document).ready(function() {
 	getMyBox();
@@ -18,8 +18,6 @@ function setList(monsterID,monsterLevel,bid){
 		"<a class='myButton' onclick='setLeader("+monsterID+","+monsterLevel+","+bid+")'>設為隊長</a>",
 		html:true, 
 		cancelButtonText: "取消",
-	},function(){
-		
 	});
 }
 //{"name": "\u5c0f\u767d\u55b5", "level": 1, "attribute": 1, "bid": 244, "mid": 1, "capital": true}
@@ -53,29 +51,30 @@ function eatMonster(monsterID,eatBid){
 
 function sentToServer(eatBid,eatedBid){
 	//送合成訊息給server
+	
 	$.ajax({
-				type:"GET",
-				url:serverUrlEatMonster,
-				data:"session="+localStorage.session+"&eatedMonster="+eatedBid+"&eatMonster="+eatBid,
-				dataType:"JSONP",
-				jsonpCallback:"combineMonster",
-				success:function(returnData){
-					swal("合成成功","success");
-					swal({   
-						title: "合成成功",   
-						type:"success",  
-						confirmButtonText: "確定",
-						closeOnConfirm:true
-					},
-					function(isconfirm){
-						window.location.reload();
-					});
-				},
+		type:"GET",
+		url:serverUrlEatMonster,
+		data:"session="+localStorage.session+"&eatedMonster="+eatedBid+"&eatMonster="+eatBid,
+		dataType:"JSONP",
+		jsonpCallback:"combineMonster",
+		success:function(returnData){
+			swal("合成成功","success");
+			swal({   
+				title: "合成成功",   
+				type:"success",  
+				confirmButtonText: "確定",
+				closeOnConfirm:true
+			},
+			function(isconfirm){
+				window.location.reload();
 			});
+		},
+	});
 }
 var bugRelease=true;
 function releaseMonster(monsterID,bid){
-	
+	bugRelease=true;
 	if(bugRelease===true){
 	swal({   
 		title: "確認放生?",   
@@ -83,24 +82,31 @@ function releaseMonster(monsterID,bid){
 		imageUrl: "img/monster"+monsterID+".png", 
 		confirmButtonText: "確定",
 		cancelButtonText: "取消",
-		closeOnConfirm:true
+		closeOnConfirm:false,
 	},
 	function(isconfirm){
 		//andy-lin.info:20003/api/api/api/releaseMonster?session=使用者id&bid=BJ4
 		
 		if (isconfirm) {
-			
-			$.ajax({
-				type:"GET",
-				url:serverUrlReleaseMonster,
-				data:"session="+localStorage.session+"&bid="+bid,
-				dataType:"JSONP",
-				jsonpCallback:"releaseMonster",
-				success:function(returnData){
-					window.location.reload();
-					
-				},
-			});
+			if(bid==leaderbid){
+				swal({
+					title:"隊長不能放生",
+					confirmButtonText: "確定",
+					type:"warning"
+				});
+			}else{
+				$.ajax({
+					type:"GET",
+					url:serverUrlReleaseMonster,
+					data:"session="+localStorage.session+"&bid="+bid,
+					dataType:"JSONP",
+					jsonpCallback:"releaseMonster",
+					success:function(returnData){
+						window.location.reload();
+						
+					},
+				});
+			}
 			
 		}
 	});
@@ -110,34 +116,41 @@ function releaseMonster(monsterID,bid){
 
 }
 function setLeader(monsterID,monsterLevel,bid){
-		
-	swal({   
-		title: "設成隊長?",   
-		showCancelButton: true,   
-		imageUrl: "img/monster"+monsterID+".png", 
-		confirmButtonText: "更換",
-		cancelButtonText: "取消",
-		closeOnConfirm:true
-	},
-	function(isconfirm){
-		//andy-lin.info:20003/api/api/setCapital?session=使用者id&bid=BJ4
+	if(bid==leaderbid){
+		swal({
+			title:"已經是隊長",
+			confirmButtonText: "確定",
+			
+		});
+	}else{
+		swal({   
+			title: "設成隊長?",   
+			showCancelButton: true,   
+			imageUrl: "img/monster"+monsterID+".png", 
+			confirmButtonText: "更換",
+			cancelButtonText: "取消",
+			closeOnConfirm:true
+		},
+		function(isconfirm){
+			//andy-lin.info:20003/api/api/setCapital?session=使用者id&bid=BJ4
 
-		if (isconfirm) {
-			localStorage.leader=monsterID;
-			localStorage.leaderLV=monsterLevel;
-			$.ajax({
-				type:"GET",
-				url:serverUrlSetLeader,
-				data:"session="+localStorage.session+"&bid="+bid,
-				dataType:"JSONP",
-				jsonpCallback:"setCapital",
-				success:function(returnData){
-					window.location.reload();
-					
-				},
-			});
-		}
-	});
+			if (isconfirm) {
+				localStorage.leader=monsterID;
+				localStorage.leaderLV=monsterLevel;
+				$.ajax({
+					type:"GET",
+					url:serverUrlSetLeader,
+					data:"session="+localStorage.session+"&bid="+bid,
+					dataType:"JSONP",
+					jsonpCallback:"setCapital",
+					success:function(returnData){
+						window.location.reload();
+						
+					},
+				});
+			}
+		});
+	}
 }
 
 function getMyBox(){
@@ -155,6 +168,7 @@ function getMyBox(){
 					var monsHp=monsterList[bagList[i].mid].monsterHP+bagList[i].level*monsterList[bagList[i].mid].HPCoe;	
 					var monsAttack=monsterList[bagList[i].mid].monsterAttack+bagList[i].level*monsterList[bagList[i].mid].AttackCoe;	
 					if(bagList[i].capital==true){
+						leaderbid=bagList[i].bid;
 						$("#boxTable").append("	<tr class='pet' onclick='setList("+bagList[i]["mid"]+","+bagList[i]["level"]+","+bagList[i]["bid"]+")' ><td style='background:red;'>"+
 							"<img src='img/monster"+bagList[i].mid+".png'  style='height: 100px; width: auto;'>"
 						+"</td><td>"+bagList[i]["name"]+"</td><td>"
